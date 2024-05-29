f467f6e58b4089642f00243259d69bf39a0a63ed
PracLee <mrgoga@naver.com>
2024-05-29 18:39:50 +0900

    no message

---
+++ app/src/main/java/codingtest/app/day06/Day09_2.java
@@ -0,0 +1,23 @@
+package codingtest.app.day06;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때
+ * 가장 중앙에 위치하는 값을 의미합니다.
+ * 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다.
+ * 정수 배열 array가 매개변수로 주어질 때,
+ * 중앙값을 return 하도록 solution 함수를 완성해보세요.
+ */
+public class Day09_2 {
+    public int solution(int[] array) {
+        Integer[] integers = Arrays.stream(array)
+                .boxed().toArray(Integer[]::new);
+        List<Integer> integerList = Arrays.asList(integers);
+
+        Collections.sort(integerList);
+        return integerList.get(integerList.size() / 2);
+    }
+}
---
+++ app/src/main/java/codingtest/app/day06/Day09_4.java
@@ -0,0 +1,18 @@
+package codingtest.app.day06;
+
+/**
+ * 정수 n이 매개변수로 주어질 때,
+ * n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+ */
+public class Day09_4 {
+    public int[] solution(int n) {
+        int size = n % 2 == 0 ? n / 2 : (n / 2) + 1;
+        int[] answer = new int[size];
+        int val = 1;
+        for (int idx = 0; idx < size; idx++) {
+            answer[idx] = val;
+            val = val + 2;
+        }
+        return answer;
+    }
+}
